6c8e121ae6fe50081720bdfc7504f6f42b804cbe
Pire Svyt <savoyat.p@gmail.com>
2024-02-12 06:10:12 +0100

    ok

--- features/usability.steps.js
+++ features/usability.steps.js
@@ -1,13 +1,12 @@
-const { expect } = require('@playwright/test')
-const { Given, When, Then } = require('@cucumber/cucumber')
+const { expect } = require("@playwright/test")
+const { Given, When, Then } = require("@cucumber/cucumber")
+const { AxeBuilder } = require('@axe-core/playwright')
 
-Then('usability is acceptable', async () => {
-    const accessibilityScanResults = await new AxeBuilder(
-        await global.page
-    ).analyze()
+Then("usability is acceptable", async () => {
+    const accessibilityScanResults = await new AxeBuilder( await global.page ).analyze();
     await testInfo.attach('accessibility-scan-results', {
         body: JSON.stringify(accessibilityScanResults, null, 2),
-        contentType: 'application/json',
-    })
-    expect(accessibilityScanResults.violations).toEqual([])
+        contentType: 'application/json'
+    });
+    expect(accessibilityScanResults.violations).toEqual([]);
 })
